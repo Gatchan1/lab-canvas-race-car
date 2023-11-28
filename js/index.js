7462e41aa663d0f42796dc057ee6e203c6d75509
Gatchan1 <r.barrio.f@gmail.com>
2023-11-28 11:01:26 +0100

    code refactoring

--- js/index.js
+++ js/index.js
@@ -1,139 +1,112 @@
-let road = document.createElement("img");
-road.src = "images/road.png";
-    
-let player = document.createElement("img");
-player.src = "images/car.png";
-let xPlayer = 230;
-let score = -60;
-
-
-
-const canvasObj = {
+const game = {
   canvas: null,
   ctx: null,
   intervalId: null,
-
+  road: document.createElement("img"),
+  player: document.createElement("img"),
+  xPlayer: 230,
+  score: -60,
+  frames: 0,
   start: function () {
     this.canvas = document.getElementById("canvas");
     this.ctx = this.canvas.getContext("2d");
-    this.intervalId = setInterval(update,60);
+    this.intervalId = setInterval(update, 60);
+    this.road.src = "images/road.png";
+    this.player.src = "images/car.png";
   },
-
   clear: function () {
-    this.ctx.clearRect(0,0,500,700)
+    this.ctx.clearRect(0, 0, 500, 700);
   },
-
   stop: function () {
-    clearInterval(this.intervalId)
+    clearInterval(this.intervalId);
   },
-}
-
-function playerPrint() {
-  document.getElementById("canvas").getContext("2d").drawImage(player, xPlayer, 580, 40, 80);
-}
-
-function scorePrint(num) {
-  let textToShow;
-  if (num < 0) {
-    textToShow = "Score: 0";
-  } else textToShow = `Score: ${num}`
-  document.getElementById("canvas").getContext("2d").fillStyle = "white";
-  document.getElementById("canvas").getContext("2d").font = "20px Arial"
-  document.getElementById("canvas").getContext("2d").fillText(textToShow, 20, 20)
-}
-
-
-
+  playerPrint: function () {
+    this.ctx.drawImage(this.player, this.xPlayer, 580, 40, 80);
+  },
+  roadPrint: function () {
+    this.ctx.drawImage(game.road, 0, 0, 500, 700);
+  },
+  scorePrint: function (num) {
+    let textToShow;
+    if (num < 0) {
+      textToShow = "Score: 0";
+    } else textToShow = `Score: ${num}`;
+    this.ctx.fillStyle = "white";
+    this.ctx.font = "20px Arial";
+    this.ctx.fillText(textToShow, 20, 20);
+  },
+};
 
 class Rectangle {
-  constructor (x,w) {
+  constructor(x, w) {
     this.x = x;
     this.y = -40;
     this.w = w;
-    this.h = 40;    
+    this.h = 40;
   }
-
   recalculatePosition(moveY) {
     this.y += moveY;
-    
   }
-
   print() {
-    canvasObj.ctx.fillStyle = "red";
-    canvasObj.ctx.fillRect(this.x,this.y,this.w,this.h)
+    game.ctx.fillStyle = "red";
+    game.ctx.fillRect(this.x, this.y, this.w, this.h);
   }
 }
 
-
-
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    canvasObj.start();
-  };    
-}
-
-
-let obstacles = [];
-let i=0;
+const obstacles = {
+  array: [],
+  createObstacles: function (framesBetweenObstacles) {
+    if (game.frames % framesBetweenObstacles == 0) {
+      game.score += 20;
+      let x = Math.random() * 400;
+      let w = Math.random() * 300;
+      let newObstacle = new Rectangle(x, w);
+      this.array.push(newObstacle);
+    }
+  },
+  recalculatePosition: function () {
+    this.array.forEach((obstacle) => {
+      obstacle.recalculatePosition(10);
+      if (!(obstacle.y + obstacle.h < 580 || obstacle.x + obstacle.w < game.xPlayer || game.xPlayer + 40 < obstacle.x || 580 + 80 < obstacle.y)) {
+        game.stop();
+      }
+    });
+  },
+  print: function () {
+    this.array.forEach((obstacle) => obstacle.print());
+  },
+};
 
 const update = () => {
-  i++;
-  
-  //limpiar
-  canvasObj.clear();
+  game.frames++;
+  game.clear();
+  // update
+  obstacles.createObstacles(25);
+  obstacles.recalculatePosition();
+  // redibujar
+  game.roadPrint();
+  obstacles.print();
+  game.playerPrint();
+  game.scorePrint(game.score);
+};
 
-  //redibujar
-  // 1-carretera
-  document.getElementById("canvas").getContext("2d").drawImage(road, 0, 0, 500, 700);
-  // 2-obstaculos
-
-//meter aqui el draw de los obstaculos
-  if (i%20 == 0) {
-    score += 20;
-
-    let x = Math.random() * 400;
-    let w = Math.random() * 300;
-    
-    let newObstacle = new Rectangle (x,w);
-
-    obstacles.push(newObstacle);
-  }
-
-  obstacles.forEach((obstacle) => {
-    obstacle.y += 10;
-
-    if(!(((obstacle.y + obstacle.h) < 580)||(obstacle.x + obstacle.w < xPlayer)||((xPlayer + 40) < obstacle.x)||((580 + 80) < obstacle.y))) {
-      canvasObj.stop();
-    }
-
-    obstacle.print();
-  })
-  
-
-  // 3-jugador
-  playerPrint()
-  
-  scorePrint(score)
-
-}
-
+window.onload = () => {
+  document.getElementById("start-button").onclick = () => {
+    game.start();
+  };
+};
 
 document.body.addEventListener("keydown", (e) => {
   if (e.key == "ArrowLeft") {
-    xPlayer -= 20
+    game.xPlayer -= 20;
   }
   if (e.key == "ArrowRight") {
-    xPlayer += 20
+    game.xPlayer += 20;
   }
-
-
-  if (xPlayer < 0) {
-    xPlayer = 0;
+  if (game.xPlayer < 0) {
+    game.xPlayer = 0;
   }
-  if (xPlayer > 460) {
-    xPlayer = 460;
+  if (game.xPlayer > 460) {
+    game.xPlayer = 460;
   }
-})
-
-
-// Me he guiado casi al 100% por el código que hizo Mariona para el codealong del flappy bird explicándonoslo paso a paso
+});
